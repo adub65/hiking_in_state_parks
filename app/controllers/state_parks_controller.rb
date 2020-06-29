ed70e8ac489cb9a02748e42868943bf4715d116f
Aaron Wilson <adub65@gmail.com>
2020-06-29 10:50:35 -0400

    set up state parks controller and views for new and create

--- app/controllers/state_parks_controller.rb
+++ app/controllers/state_parks_controller.rb
@@ -1,5 +1,18 @@
 class StateParksController < ApplicationController
 
+  def index
+    @state_parks = StateParks.All
+  end
+
+  def new
+    @state_park = StatePark.new
+  end
+
+  def create
+    state_park = StatePark.create(state_park_params)
+    redirect_to state_park_path(state_park)
+  end
+
 private
 
   def state_park_params
