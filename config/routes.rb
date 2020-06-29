c0348730a28f79b5675e7ef9aa7cbb9671b1d426
Aaron Wilson <adub65@gmail.com>
2020-06-29 09:03:01 -0400

    set up sessions and static routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  root "static#welcome"
 
   resources :state_parks
   resources :trails
   resources :users
   resources :hikes
 
+  get "/signin", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  
+  get "/logout", to: "sessions#destroy"
 end
