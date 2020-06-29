7895b7d7ff72d4896791519eeab8865ce68afaf3
Aaron Wilson <adub65@gmail.com>
2020-06-29 09:00:26 -0400

    copied sessions controller from old lab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    session[:user_id] = user.id
+
+    redirect_to user_path(user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_path unless session[:user_id].present?
+  end
 
 private
 
